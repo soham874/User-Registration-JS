6dffced502ceac187ad1144adaf649e27e723ac1
soham874 <soham874@gmail.com>
2020-12-21 21:36:06 +0530

    Added : Verification for user last name input

--- Utility.js
+++ Utility.js
@@ -1,6 +1,7 @@
 var readLineSunc = require('readline-sync')
 
 class Utility {
+
     //verify the first name
     firstNameVerification(){
         let name = readLineSunc.question("What is your first name ? : ");
@@ -13,14 +14,28 @@ class Utility {
         }
             
         console.log("Error in input. Please try again.")
-        this.firstNameVerification()
-        
+        this.firstNameVerification()        
+    }
+
+    //verify the last name
+    lastNameVerification(){
+        let name = readLineSunc.question("What is your last name ? : ");
+
+        var patternName = RegExp('^[A-Z][a-z]{2,}$','gm')
+        var test = patternName.test(name)
+        if( test == true){
+            console.log("Last Name is according to pattern")
+            return 0
+        }
             
+        console.log("Error in input. Please try again.")
+        this.lastNameVerification()        
     }
 
     //take details input and call corresponding functions
     inputDetails(){
         this.firstNameVerification()
+        this.lastNameVerification()
         
     }
 
