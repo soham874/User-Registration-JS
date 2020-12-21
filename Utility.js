071b116e0940b5db73641e87baf2579f5d4eb235
soham874 <soham874@gmail.com>
2020-12-21 21:55:52 +0530

    Added : password minimum 8 charecters

--- Utility.js
+++ Utility.js
@@ -36,8 +36,8 @@ class Utility {
     emailVerification() {
         let name = readLineSunc.question("What is your email ID ? : ");
 
-        var patternName = RegExp('^[a-z0-9]+([._+-][a-z0-9]+)*(@)[0-9a-zA-Z]+[.]{1}[a-z]{2,3}([.][a-z]{2})?$', 'gm')
-        var test = patternName.test(name)
+        var patternEmail = RegExp('^[a-z0-9]+([._+-][a-z0-9]+)*(@)[0-9a-zA-Z]+[.]{1}[a-z]{2,3}([.][a-z]{2})?$', 'gm')
+        var test = patternEmail.test(name)
         if (test == true) {
             console.log("Email ID is according to pattern")
             return 0
@@ -51,8 +51,8 @@ class Utility {
     phoneNumberVerification(){
         let name = readLineSunc.question("What is your phone number ? : ");
 
-        var patternName = RegExp('[0-9]{2}[ ][0-9]{10}$','gm')
-        var test = patternName.test(name)
+        var patternPhoneNumber = RegExp('^[0-9]{2}[ ][0-9]{10}$','gm')
+        var test = patternPhoneNumber.test(name)
         if( test == true){
             console.log("Phone number is according to pattern")
             return 0
@@ -62,12 +62,28 @@ class Utility {
         this.phoneNumberVerification()        
     }
 
+     //verify password
+    passwordVerification(){
+        let name = readLineSunc.question("What is your password ? : ");
+
+        var patternName = RegExp('.{8,}')
+        var test = patternName.test(name)
+        if( test == true){
+            console.log("Password is according to pattern")
+            return 0
+        }
+            
+        console.log("Error in input. Please try again.")
+        this.passwordVerification()        
+    }
+
     //take details input and call corresponding functions
     inputDetails() {
         this.firstNameVerification()
         this.lastNameVerification()
         this.emailVerification()
         this.phoneNumberVerification()
+        this.passwordVerification()   
     }
 
 }
